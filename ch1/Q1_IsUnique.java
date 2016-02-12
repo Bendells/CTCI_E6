421cf025099461f506f1eb9a5be91c88c38dbdf2
Slimane Bendelloul <slimane@eraid.biz>
2016-02-12 00:04:29 -0500

    Q1.1

---
+++ ch1/Q1_IsUnique.java
@@ -0,0 +1,41 @@
+package ch1;
+
+import java.util.*;
+
+public class Q1_IsUnique {
+	
+	public static boolean isUnique(String input){
+		/*<--------- Strategy -----------
+		 * 1. We need to make sure that every charcter in the string is unique
+		 *  |--> by making sure that the alphabet (128 bit ascii table) is there only once,
+		 *  	 we assure that the string is valid, else it fails to comply
+		 */
+		char[] array = new char[128];
+		
+		for(int i = 0; i < input.length(); i++){
+			char charToCheck = input.charAt(i);
+			
+			if(array[charToCheck] == 0){
+				array[charToCheck]++; 
+			}
+			else// we already encountred that character
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args){
+		
+		String[] stringsToTest = {"ascrfgy", "dswqPFD", "PpOoKk", "ooOO", "baby"};
+		String result;
+		for(String string : stringsToTest){
+			
+			result = (isUnique(string) ) ? "VALID" : "NOT VALID";
+			System.out.println("The string \""+ string + "\" is " + result);
+		}
+		
+	}
+	
+
+}
